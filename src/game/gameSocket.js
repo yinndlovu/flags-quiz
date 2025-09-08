a00eae66c40a58f12d474461483602c215639420
Yin <yinnndlovu@gmail.com>
2025-09-08 11:34:50 +0200

    feat: add a more forgiving logic for answering the trivia questions

--- src/game/gameSocket.js
+++ src/game/gameSocket.js
@@ -4,6 +4,90 @@ const countries = require("../data/countries");
 let queue = [];
 let games = new Map();
 
+function normalizeName(s) {
+  if (!s) {
+    return "";
+  }
+
+  return s
+    .toLowerCase()
+    .normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+    .replace(/\([^)]*\)/g, " ")
+    .replace(/^[\s]*the\s+/, "")
+    .replace(/[^a-z0-9\s]/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+}
+
+const ALIASES = new Map([
+  ["uk", "united kingdom"],
+  ["u k", "united kingdom"],
+  ["great britain", "united kingdom"],
+  ["britain", "united kingdom"],
+
+  ["usa", "united states of america"],
+  ["u s a", "united states of america"],
+  ["us", "united states of america"],
+  ["u s", "united states of america"],
+  ["united states", "united states of america"],
+
+  ["uae", "united arab emirates"],
+  ["u a e", "united arab emirates"],
+
+  ["netherlands", "netherlands"],
+  ["bahamas", "bahamas"],
+  ["gambia", "gambia"],
+  ["cabo verde", "cabo verde"],
+  ["cape verde", "cabo verde"],
+
+  ["ivory coast", "cote divoire"],
+  ["cote divoire", "cote divoire"],
+
+  ["czech republic", "czechia"],
+  ["russia", "russian federation"],
+  ["south korea", "korea republic of"],
+  ["north korea", "korea democratic peoples republic of"],
+  ["swaziland", "eswatini"],
+  ["vatican", "holy see"],
+  ["bolivia", "bolivia plurinational state of"],
+  ["moldova", "moldova republic of"],
+  ["laos", "lao peoples democratic republic"],
+  ["brunei", "brunei darussalam"],
+  ["iran", "iran islamic republic of"],
+  ["tanzania", "tanzania united republic of"],
+  ["palestine", "palestine state of"],
+  ["syria", "syrian arab republic"],
+  ["macedonia", "republic of north macedonia"],
+]);
+
+function canonicalize(s) {
+  const n = normalizeName(s);
+  return ALIASES.get(n) || n;
+}
+
+function isAnswerCorrect(answer, correct) {
+  const a = canonicalize(answer);
+  const c = canonicalize(correct);
+
+  if (!a || !c) {
+    return false;
+  }
+  
+  if (a === c) {
+    return true;
+  }
+
+  if (a.length >= 3 && new RegExp(`\\b${a.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}\\b`).test(c)) {
+    return true;
+  }
+
+  if (c.length >= 3 && new RegExp(`\\b${c.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}\\b`).test(a)) {
+    return true;
+  }
+
+  return false;
+}
+
 function generateQuestions(num) {
   const selectedIndices = [];
   while (selectedIndices.length < num) {
@@ -27,7 +111,7 @@ function startNextQuestion(gameId) {
   const question = game.questions[game.currentQuestion];
   const flagUrl = `https://flagsapi.com/${question.code}/flat/64.png`;
 
-  game.players[0].io.to(gameId).emit("newQuestion", {
+  game.io.to(gameId).emit("newQuestion", {
     flagUrl,
     questionNum: game.currentQuestion + 1,
   });
@@ -40,12 +124,12 @@ function startNextQuestion(gameId) {
         return;
       }
 
-      if (ans.toLowerCase().trim() === question.name.toLowerCase()) {
+      if (isAnswerCorrect(ans, question.name)) {
         answered = true;
         clearTimeout(timeout);
         cleanup();
         game.scores[index]++;
-        game.players[0].io.to(gameId).emit("correctAnswer", {
+        game.io.to(gameId).emit("correctAnswer", {
           playerId: player.id,
           correct: question.name,
         });
@@ -68,7 +152,7 @@ function startNextQuestion(gameId) {
   const timeoutCallback = () => {
     if (!answered) {
       cleanup();
-      game.players[0].io.to(gameId).emit("timeUp", { correct: question.name });
+      game.io.to(gameId).emit("timeUp", { correct: question.name });
       game.currentQuestion++;
       startNextQuestion(gameId);
     }
@@ -89,7 +173,7 @@ function endGame(gameId) {
     player2: { id: game.players[1].id, score: game.scores[1] },
   };
 
-  game.players[0].io.to(gameId).emit("gameEnd", results);
+  game.io.to(gameId).emit("gameEnd", results);
   games.delete(gameId);
 }
 
@@ -104,7 +188,7 @@ module.exports = (server) => {
   io.on("connection", (socket) => {
     socket.on("joinGame", () => {
       queue.push(socket);
-      
+
       if (queue.length >= 2) {
         const player1 = queue.shift();
         const player2 = queue.shift();
